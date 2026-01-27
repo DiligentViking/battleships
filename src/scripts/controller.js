6f862086d5f669baf80745246445d346a6b56a08
DiligentViking <acore-n@proton.me>
2026-01-27 15:35:00 -0600

    Add board attacking functionality

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -1,10 +1,21 @@
 export function Controller(player1, player2, view) {
   return {
     init() {
-      const board1 = player1.gameboard.getBoard();
-      const board2 = player2.gameboard.getBoard();
+      let board1 = player1.gameboard.getBoard();
+      let board2 = player2.gameboard.getBoard();
       view.renderBoard(board1, 1);
       view.renderBoard(board2, 2);
+
+      let turn = 1;
+      const {p1Board, p2Board} = view.eventElems;
+      p1Board.addEventListener('click', (e) => {
+        if (e.target.tagName === 'BUTTON') {
+          const coords = e.target.dataset.coord.split(',');
+          player1.gameboard.receiveAttack(coords);
+          board1 = player1.gameboard.getBoard();
+          view.renderBoard(board1, 1);
+        }
+      });
     },
   }
 }
--- src/scripts/view.js
+++ src/scripts/view.js
@@ -2,15 +2,17 @@ export function View(root) {
   const p1Board = root.querySelector(".p1-board");
   const p2Board = root.querySelector(".p2-board");
   return {
+    eventElems: {p1Board, p2Board},
     renderBoard(board, playerNum) {
       const shipIcons = [" ", "🛶", "🛥️", "⛵", "🛳️", "⛴️", "🚢"];
       const boardElem = playerNum === 1 ? p1Board : p2Board;
+      boardElem.textContent = '';
       for (let i = 0; i < board.length; i++) {
         const row = board[i];
         for (let j = 0; j < row.length; j++) {
           const cell = board[i][j];
           const cellElem = document.createElement("button");
-          cellElem.textContent = shipIcons[cell];
+          cellElem.textContent = shipIcons[cell] ?? cell;
           cellElem.dataset.coord = `${i},${j}`;
           boardElem.appendChild(cellElem);
         }

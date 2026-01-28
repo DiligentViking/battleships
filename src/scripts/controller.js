9547f119578e9bedc391385e5c0d40e0f3b4f64d
DiligentViking <acore-n@proton.me>
2026-01-28 10:57:38 -0600

    Polish controller logic

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -1,44 +1,28 @@
 export function Controller(player1, player2, view) {
   return {
     init() {
-      let board1 = player1.gameboard.getBoard();
-      let board2 = player2.gameboard.getBoard();
-      view.renderBoard(board1, 1);
-      view.renderBoard(board2, 2);
+      view.renderBoard(player1.gameboard.getBoard(), 1);
+      view.renderBoard(player2.gameboard.getBoard(), 2);
 
-      function onBoardClick(playerNum) {
-        const boardElem = playerNum === 1 ? p1Board : p2Board;
+      this.runGame();
+    },
+
+    runGame() {
+      function attackCell(cellElem, playerNum) {
         const player = playerNum === 1 ? player1 : player2;
-        boardElem.addEventListener("click", (e) => {
-          if (e.target.tagName === "BUTTON") {
-            const coords = e.target.dataset.coord.split(",");
-            player.gameboard.receiveAttack(coords);
-            const board = player.gameboard.getBoard();
-            view.renderBoard(board, playerNum);
-            if (turn === 1) {
-              turn = 2;
-              if (player2.type === "computer") {
-                const move =
-                  computerMoves[Math.round(Math.random() * computerMoves.length)];
-                document
-                  .querySelector(
-                    `.p1-board > [data-coord='${move[0]},${move[1]}']`,
-                  )
-                  .click();
-                turn = 1;
-              }
-            }
-          }
-        });
+        const coords = cellElem.dataset.coord.split(",");
+        const board = player.gameboard.getBoard();
+
+        player.gameboard.receiveAttack(coords);
+        view.renderBoard(board, playerNum);
       }
 
       let turn = 1;
       const { p1Board, p2Board } = view.eventElems;
-      onBoardClick(1);
-      onBoardClick(2);
 
       const computerMoves = [];
       if (player2.type === "computer") {
+        const board1 = player1.gameboard.getBoard();
         for (let i = 0; i < board1.length; i++) {
           const row = board1[i];
           for (let j = 0; j < row.length; j++) {
@@ -46,6 +30,31 @@ export function Controller(player1, player2, view) {
           }
         }
       }
+
+      p2Board.addEventListener("click", (e) => {
+        if (e.target.tagName !== "BUTTON") return;
+        if (turn !== 1) return;
+
+        attackCell(e.target);
+
+        turn = 2;
+        if (player2.type !== "computer") return;
+        const move =
+          computerMoves[Math.round(Math.random() * computerMoves.length)];
+        const cellToAttack = document.querySelector(
+          `.p1-board > [data-coord='${move[0]},${move[1]}']`,
+        );
+        cellToAttack.click();
+      });
+
+      p1Board.addEventListener("click", (e) => {
+        if (e.target.tagName !== "BUTTON") return;
+        if (turn !== 2) return;
+
+        attackCell(e.target);
+
+        turn = 1;
+      });
     },
   };
 }

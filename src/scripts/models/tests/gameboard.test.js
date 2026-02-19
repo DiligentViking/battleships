71f4b608f763ee74eae38668982704261687a07c
DiligentViking <acore-n@proton.me>
2026-02-19 14:02:45 -0600

    Fix gameboard test issue from magic-num refactor

--- src/scripts/models/tests/gameboard.test.js
+++ src/scripts/models/tests/gameboard.test.js
@@ -3,15 +3,15 @@ import { Gameboard } from "../gameboard";
 test("adds a ship", () => {
   const gameboard = Gameboard();
 
-  gameboard.placeShip(1, 3, [1, 5]);
+  gameboard.placeShip(0, 3, [1, 5]);
   const ships = gameboard.getShips();
-  expect(ships[1].length).toBe(3);
+  expect(ships[0].length).toBe(3);
 });
 
 test("places a ship horizontally at a coordinate", () => {
   const gameboard = Gameboard();
 
-  gameboard.placeShip(1, 3, [1, 5]);
+  gameboard.placeShip(0, 3, [1, 5]);
   const board = gameboard.getBoard();
   expect(board[1][5]).not.toBe(0);
   expect(board[1][6]).not.toBe(0);
@@ -20,44 +20,44 @@ test("places a ship horizontally at a coordinate", () => {
 test("does not place ship out of bounds", () => {
   const gameboard = Gameboard();
 
-  expect(() => gameboard.placeShip(1, 3, [1, 8])).toThrow(Error);
+  expect(() => gameboard.placeShip(0, 3, [1, 8])).toThrow(Error);
 });
 test("does not place ship in already taken cells", () => {
   const gameboard = Gameboard();
 
-  gameboard.placeShip(1, 3, [1, 4]);
+  gameboard.placeShip(0, 3, [1, 4]);
   expect(() => gameboard.placeShip(2, 5, [1, 2])).toThrow(Error);
 });
 
 test("increments ship hits when a ship cell is hit", () => {
   const gameboard = Gameboard();
-  gameboard.placeShip(1, 3, [1, 2]);
+  gameboard.placeShip(0, 3, [1, 2]);
 
   gameboard.receiveAttack([1, 3]);
   const ships = gameboard.getShips();
-  expect(ships[1].hits).toBe(1);
+  expect(ships[0].hits).toBe(1);
 });
 test("marks ship cell when the cell is hit", () => {
   const gameboard = Gameboard();
-  gameboard.placeShip(1, 3, [1, 2]);
+  gameboard.placeShip(0, 3, [1, 2]);
 
   gameboard.receiveAttack([1, 3]);
   const board = gameboard.getBoard();
-  expect(board[1][3]).toEqual({"hit": true, "shipID": 1});
+  expect(board[1][3]).toEqual({ hit: true, shipID: 0 });
 });
 test("marks empty cell when the cell is hit", () => {
   const gameboard = Gameboard();
-  gameboard.placeShip(1, 3, [1, 2]);
+  gameboard.placeShip(0, 3, [1, 2]);
 
   gameboard.receiveAttack([2, 3]);
   const board = gameboard.getBoard();
-  expect(board[2][3]).toEqual({"hit": true, "shipID": 0});
+  expect(board[2][3]).toEqual({ hit: true, shipID: null });
 });
 
 test("knows if all ships are sunk", () => {
   const gameboard = Gameboard();
-  gameboard.placeShip(1, 1, [1, 4]);
-  gameboard.placeShip(2, 2, [2, 4]);
+  gameboard.placeShip(0, 1, [1, 4]);
+  gameboard.placeShip(1, 2, [2, 4]);
 
   gameboard.receiveAttack([1, 4]);
   gameboard.receiveAttack([2, 4]);

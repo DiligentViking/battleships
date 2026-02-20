d794d8be7dbd2d829f03252de6b3c22230c01f2e
DiligentViking <acore-n@proton.me>
2026-02-20 10:47:37 -0600

    Move attack method and ai logic into player.js

--- src/scripts/models/game.js
+++ src/scripts/models/game.js
@@ -3,17 +3,18 @@ export function Game(player1, player2) {
 
   return {
     attack(coords) {
-      const enemyPlayer = turn === 1 ? player2 : player1;
+      const attacker = turn === 1 ? player1 : player2;
+      const receiver = turn === 1 ? player2 : player1;
       
-      enemyPlayer.gameboard.receiveAttack(coords);
+      attacker.attack(receiver.gameboard, coords);
       turn = turn === 1 ? 2 : 1;
     },
 
     getState() {
-      const attacker = turn === 1 ? player2 : player1;
-      const receiver = turn === 1 ? player1 : player2;
+      const wasAttacker = turn === 1 ? player2 : player1;
+      const wasReceiver = turn === 1 ? player1 : player2;
 
-      const winner = receiver.gameboard.areAllShipsSunk() ? attacker.getName() : null;
+      const winner = wasReceiver.gameboard.areAllShipsSunk() ? wasAttacker.getName() : null;
 
       return { turn, winner };
     },
--- src/scripts/models/player.js
+++ src/scripts/models/player.js
@@ -3,6 +3,18 @@ import { Gameboard } from "./gameboard.js";
 export function Player(name, type) {
   const gameboard = Gameboard();
 
+  function chooseRandomCoords(board) {
+    const coords = [
+      Math.floor(Math.random() * board.getBoardHeight()),
+      Math.floor(Math.random() * board.getBoardWidth()),
+    ];
+    if (board.getCellHit(coords)) {
+      return chooseRandomCoords(board);
+    } else {
+      return coords;
+    }
+  }
+
   return {
     gameboard,
 
@@ -12,6 +24,13 @@ export function Player(name, type) {
 
     getType() {
       return type;
+    },
+
+    attack(enemyGameboard, coords) {
+      if (type === "computer") {
+        coords = chooseRandomCoords(enemyGameboard);
+      }
+      enemyGameboard.receiveAttack(coords);
     },
   };
 }

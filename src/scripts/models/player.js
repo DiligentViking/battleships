5e091e955acc7313065978a317710faf0c3606b2
DiligentViking <acore-n@proton.me>
2026-02-24 11:01:08 -0600

    Replace old AI random selection logic

--- src/scripts/models/player.js
+++ src/scripts/models/player.js
@@ -3,18 +3,6 @@ import { Gameboard } from "./gameboard.js";
 export function Player(name, type) {
   const gameboard = Gameboard();
 
-  function chooseRandomCoords1(board) {
-    const coords = [
-      Math.floor(Math.random() * board.getBoardHeight()),
-      Math.floor(Math.random() * board.getBoardWidth()),
-    ];
-    if (board.getCellHit(coords)) {
-      return chooseRandomCoords(board); // TODO: Make it where it chooses from available cells instead
-    } else {
-      return coords;
-    }
-  }
-
   function chooseRandomCoords(board) {
     const choices = [];
     board.getBoard().forEach((row, i) =>
@@ -23,7 +11,7 @@ export function Player(name, type) {
       }),
     );
     const randomIndex = Math.floor(Math.random() * choices.length);
-    console.log(choices[randomIndex]);
+    return choices[randomIndex];
   }
 
   return {
@@ -36,7 +24,6 @@ export function Player(name, type) {
     attack: (enemyGameboard, coords) => {
       if (type === "computer") {
         coords = chooseRandomCoords(enemyGameboard);
-        coords = chooseRandomCoords1(enemyGameboard);
       }
       enemyGameboard.receiveAttack(coords);
       return coords;

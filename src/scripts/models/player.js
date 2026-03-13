50ab1767551182ce7a0d8e06fc754ce5227b0d12
DiligentViking <acore-n@proton.me>
2026-03-13 10:31:17 -0500

    Add setup code; Add ai logic pseudo-code

--- src/scripts/models/player.js
+++ src/scripts/models/player.js
@@ -1,9 +1,10 @@
 import { Gameboard } from "./gameboard.js";
 
-export function Player(name, type) {
+export function Player(name, type, smartness = 0) {
   const gameboard = Gameboard();
 
   const targets = [];
+  const smartLogic = { hitCell1: null, hitCell2: null };
 
   return {
     gameboard,
@@ -22,10 +23,30 @@ export function Player(name, type) {
 
     attack: (enemyGameboard, coords) => {
       if (type === "computer") {
-        const randomTarget = Math.floor(Math.random() * targets.length);
-        coords = targets.splice(randomTarget, 1)[0];
+        if (smartness === 0) {
+          const randomTarget = Math.floor(Math.random() * targets.length);
+          coords = targets.splice(randomTarget, 1)[0];
+        } else if (smartness === 1) {
+          // PSUEDO-CODE:
+          // attack a random cellA
+          // if cellA has a ship, proceed; if not, repeat above step
+          // pop one of the four cardinal directions and attack cellB
+          // if cellB has a ship, proceed; if not, repeat above step
+          // continue attacking cells in the direction you found, until you hit a wall or empty cell
+          // go backwards attacking cells in the direction you found
+          // if after any attack the ship is sunk, stop and restart algorithm
+
+          if (smartLogic.hitCell1) {
+            ;
+          } else {
+            const randomTarget = Math.floor(Math.random() * targets.length);
+            coords = targets.splice(randomTarget, 1)[0];
+          }
+        }
       }
+
       enemyGameboard.receiveAttack(coords);
+
       return coords;
     },
   };

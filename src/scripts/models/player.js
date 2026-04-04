70da44307fc9b81b8a7d93a0af3141245d61543f
DiligentViking <acore-n@proton.me>
2026-04-03 19:01:19 -0500

    Polish smart ai code

--- src/scripts/models/player.js
+++ src/scripts/models/player.js
@@ -16,6 +16,121 @@ export function Player(name, type, smartness = 0) {
     direction: null,
   };
 
+  function makeDumbAIMove(enemyGameboard) {
+    const coords = chooseRandomItem(remainingCells);
+    enemyGameboard.receiveAttack(coords);
+    return coords;
+  }
+
+  function makeSmartAIMove(enemyGameboard) {
+    // PSUEDO-CODE:
+    // attack a random cellA
+    // if cellA has a ship, proceed; if not, repeat above step
+    // pop one of the four cardinal directions and attack cellB
+    // if cellB has a ship, proceed; if not, repeat above step
+    // continue attacking cells in the direction you found, until you hit a wall or empty/hit cell
+    // go backwards attacking cells in the direction you found
+    // if after any attack the ship is sunk, stop and restart algorithm
+
+    if (smartLogic.direction) {
+      const adjacentCells = getAdjacentCells(smartLogic.prevCell);
+      const targetCoords = adjacentCells[smartLogic.direction];
+
+      const targetIndex = findIndexOfRemainingCell(targetCoords);
+      if (targetIndex === -1) {
+        switchDirections();
+        return makeSmartAIMove(enemyGameboard);
+      }
+
+      enemyGameboard.receiveAttack(targetCoords);
+      remainingCells.splice(targetIndex, 1);
+
+      if (enemyGameboard.getCellShipIsSunk(smartLogic.startCell)) {
+        resetSmartLogicState();
+      } else if (!enemyGameboard.getCellHasShip(targetCoords)) {
+        switchDirections();
+      } else {
+        smartLogic.prevCell = targetCoords;
+      }
+
+      return targetCoords;
+    } else if (smartLogic.startCell) {
+      let direction, targetCoords, targetIndex;
+      do {
+        // Pop a direction (this whole section can probably be refactored)
+        [direction, targetCoords] = chooseRandomItem(
+          Object.entries(smartLogic.adjacentCells).filter(
+            (item) => item[1] !== null,
+          ),
+        );
+        smartLogic.adjacentCells[direction] = null;
+
+        targetIndex = findIndexOfRemainingCell(targetCoords);
+      } while (targetIndex === -1);
+
+      enemyGameboard.receiveAttack(targetCoords);
+      remainingCells.splice(targetIndex, 1);
+
+      if (enemyGameboard.getCellHasShip(targetCoords)) {
+        if (!enemyGameboard.getCellShipIsSunk(targetCoords)) {
+          smartLogic.prevCell = targetCoords;
+          smartLogic.direction = direction;
+        } else {
+          resetSmartLogicState();
+        }
+      }
+
+      return targetCoords;
+    } else {
+      const targetCoords = chooseRandomItem(remainingCells);
+
+      enemyGameboard.receiveAttack(targetCoords);
+
+      if (enemyGameboard.getCellHasShip(targetCoords)) {
+        if (!enemyGameboard.getCellShipIsSunk(targetCoords)) {
+          smartLogic.startCell = targetCoords;
+          smartLogic.adjacentCells = getAdjacentCells(targetCoords);
+        }
+      }
+
+      return targetCoords;
+    }
+  }
+
+  function resetSmartLogicState() {
+    smartLogic.startCell = null;
+    smartLogic.prevCell = null;
+    smartLogic.direction = null;
+  }
+
+  function switchDirections() {
+    smartLogic.prevCell = smartLogic.startCell;
+
+    const oppositeDirections = {
+      top: "bottom",
+      right: "left",
+      bottom: "top",
+      left: "right",
+    };
+    smartLogic.direction = oppositeDirections[smartLogic.direction];
+  }
+
+  function findIndexOfRemainingCell(coords) {
+    return remainingCells.findIndex(
+      (item) => item[0] === coords[0] && item[1] === coords[1],
+    );
+  }
+
+  function getAdjacentCells(coords) {
+    const [y, x] = coords;
+    return {
+      top: [y - 1, x],
+      right: [y, x + 1],
+      bottom: [y + 1, x],
+      left: [y, x - 1],
+    };
+  }
+
   function chooseRandomItem(array) {
     const randomIndex = Math.floor(Math.random() * array.length);
     return array.splice(randomIndex, 1)[0];
@@ -36,126 +151,10 @@ export function Player(name, type, smartness = 0) {
       }
     },
 
-    attack(enemyGameboard, coords) {
+    attack: (enemyGameboard, coords) => {
       if (type === "computer") {
-        if (smartness === 0) {
-          const coords = chooseRandomItem(remainingCells);
-          enemyGameboard.receiveAttack(coords);
-          return coords;
-        } else if (smartness === 1) {
-          // PSUEDO-CODE:
-          // attack a random cellA
-          // if cellA has a ship, proceed; if not, repeat above step
-          // pop one of the four cardinal directions and attack cellB
-          // if cellB has a ship, proceed; if not, repeat above step
-          // continue attacking cells in the direction you found, until you hit a wall or empty/hit cell
-          // go backwards attacking cells in the direction you found
-          // if after any attack the ship is sunk, stop and restart algorithm
-
-          if (smartLogic.direction) {
-            const [y, x] = smartLogic.prevCell;
-            const adjacentCells = {
-              top: [y - 1, x],
-              right: [y, x + 1],
-              bottom: [y + 1, x],
-              left: [y, x - 1],
-            };
-            const coords = adjacentCells[smartLogic.direction];
-
-            const targetIndex = remainingCells.findIndex(
-              (item) => item[0] === coords[0] && item[1] === coords[1],
-            );
-            if (targetIndex === -1) {
-              smartLogic.prevCell = smartLogic.startCell;
-
-              const oppositeDirections = {
-                top: "bottom",
-                right: "left",
-                bottom: "top",
-                left: "right",
-              };
-              smartLogic.direction = oppositeDirections[smartLogic.direction];
-
-              return this.attack(enemyGameboard);
-            }
-
-            enemyGameboard.receiveAttack(coords);
-            remainingCells.splice(targetIndex, 1);
-
-            if (enemyGameboard.getCellShipIsSunk(smartLogic.startCell)) {
-              smartLogic.startCell = null;
-              smartLogic.prevCell = null;
-              smartLogic.direction = null;
-            } else if (!enemyGameboard.getCellHasShip(coords)) {
-              smartLogic.prevCell = smartLogic.startCell;
-
-              const oppositeDirections = {
-                top: "bottom",
-                right: "left",
-                bottom: "top",
-                left: "right",
-              };
-              smartLogic.direction = oppositeDirections[smartLogic.direction];
-            } else {
-              smartLogic.prevCell = coords;
-            }
-
-            return coords;
-          } else if (smartLogic.startCell) {
-            let direction, coords, targetIndex;
-            do {
-              // Pop a direction
-              [direction, coords] = chooseRandomItem(
-                Object.entries(smartLogic.adjacentCells).filter(
-                  (item) => item[1] !== null,
-                ),
-              );
-              smartLogic.adjacentCells[direction] = null;
-
-              // Check if valid target
-              targetIndex = remainingCells.findIndex(
-                (item) => item[0] === coords[0] && item[1] === coords[1],
-              );
-            } while (targetIndex === -1);
-
-            enemyGameboard.receiveAttack(coords);
-            remainingCells.splice(targetIndex, 1);
-
-            if (enemyGameboard.getCellHasShip(coords)) {
-              if (!enemyGameboard.getCellShipIsSunk(coords)) {
-                smartLogic.prevCell = coords;
-                smartLogic.direction = direction;
-              } else {
-                smartLogic.startCell = null;
-                smartLogic.prevCell = null;
-                smartLogic.direction = null;
-              }
-            }
-
-            return coords;
-          } else {
-            const coords = chooseRandomItem(remainingCells);
-
-            enemyGameboard.receiveAttack(coords);
-
-            if (enemyGameboard.getCellHasShip(coords)) {
-              if (!enemyGameboard.getCellShipIsSunk(coords)) {
-                const [y, x] = coords;
-                smartLogic.startCell = coords;
-                smartLogic.adjacentCells.top = [y - 1, x];
-                smartLogic.adjacentCells.right = [y, x + 1];
-                smartLogic.adjacentCells.bottom = [y + 1, x];
-                smartLogic.adjacentCells.left = [y, x - 1];
-              } else {
-                smartLogic.startCell = null;
-                smartLogic.prevCell = null;
-                smartLogic.direction = null;
-              }
-            }
-
-            return coords;
-          }
-        }
+        if (smartness === 0) return makeDumbAIMove(enemyGameboard);
+        if (smartness === 1) return makeSmartAIMove(enemyGameboard);
       } else {
         enemyGameboard.receiveAttack(coords);
         return coords;
